3dbaf8522ea30fd190a757dc326f60ffa2a4b2cf
62257 <62257@LAB-CC-62257>
2016-06-23 11:59:55 +0800

    新增停車場設定

--- DoorGodWeb/DoorGodWeb/Infrastructure/CMSCardService.cs
+++ DoorGodWeb/DoorGodWeb/Infrastructure/CMSCardService.cs
@@ -25,7 +25,14 @@ namespace DoorGodWeb.Infrastructure
             //驗證EmpNo是否是正確值
             if (! Utility.ValidEmpNoList(carPassport.EmpNoList)) return "員工清單格式錯誤";
             
-            return service.AddCarPassport(DoorGodConst.GetUserName(),carPassport.SetNo,carPassport.EmpNoList,carPassport.StartDate.Value.ToString("yyyy/MM/dd"),carPassport.EndDate.Value.ToString("yyyy/MM/dd"), carPassport.CarType).ToString();
+            return service.AddCarPassport(
+                DoorGodConst.GetUserName(),
+                carPassport.SetNo,
+                carPassport.EmpNoList,
+                carPassport.StartDate.Value.ToString("yyyy/MM/dd"),
+                carPassport.EndDate.Value.ToString("yyyy/MM/dd"), 
+                carPassport.CarType
+                ).ToString();
         }        
 
         public string ClearAllCarPassport(string setNo, string carType)
